d71c49ba02f45edf64cd19a9e59fc6b1ebc39a24
nashaat10 <94199891+nashaat10@users.noreply.github.com>
2025-02-23 17:29:28 +0200

    create review schema

---
+++ src/models/reviewModel.js
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+
+const reviewSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+      required: [true, "Review can not be empty!"],
+    },
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    service: {
+      type: mongoose.Schema.ObjectId,
+      ref: "Service",
+      required: [true, "Review must belong to a service."],
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: [true, "Review must belong to a user."],
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      versionKey: false,
+      transform: function (doc, ret) {
+        delete ret._id;
+      },
+    },
+    toObject: {
+      virtuals: true,
+      versionKey: false,
+      transform: function (doc, ret) {
+        delete ret._id;
+      },
+    },
+  }
+);
+
+reviewSchema.index({ service: 1, user: 1 }, { unique: true });
+
+const Review = mongoose.model("Review", reviewSchema);
+
+export default Review;
