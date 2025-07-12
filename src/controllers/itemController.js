071a565fc104208903768be546e0b98cb52faaba
nashaat10 <94199891+nashaat10@users.noreply.github.com>
2025-07-12 21:16:28 +0300

    add item controller

---
+++ src/controllers/itemController.js
@@ -0,0 +1,77 @@
+import Item from "../models/itemModel.js";
+import catchAsync from "../utils/catchAsync.js";
+import AppError from "../utils/appError.js";
+
+export const createItem = catchAsync(async (req, res) => {
+  const newItem = await Item.create(req.body);
+  res.status(201).json({
+    status: "success",
+    data: {
+      item: newItem,
+    },
+  });
+});
+
+export const getAllItems = catchAsync(async (req, res) => {
+  const items = await Item.find();
+  res.status(200).json({
+    status: "success",
+    results: items.length,
+    data: {
+      items,
+    },
+  });
+});
+
+export const getItem = catchAsync(async (req, res, next) => {
+  const item = await Item.findById(req.params.id);
+  if (!item) {
+    return next(new AppError("No item found with that ID", 404));
+  }
+  res.status(200).json({
+    status: "success",
+    data: {
+      item,
+    },
+  });
+});
+
+export const updateItem = catchAsync(async (req, res, next) => {
+  const item = await Item.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  if (!item) {
+    return next(new AppError("No item found with that ID", 404));
+  }
+  res.status(200).json({
+    status: "success",
+    data: {
+      item,
+    },
+  });
+});
+
+export const deleteItem = catchAsync(async (req, res, next) => {
+  const item = await Item.findByIdAndDelete(req.params.id);
+  if (!item) {
+    return next(new AppError("No item found with that ID", 404));
+  }
+  res.status(204).json({
+    status: "success",
+    data: null,
+  });
+});
+export const getItemsByType = catchAsync(async (req, res, next) => {
+  const items = await Item.find({ type: req.body.type });
+  if (items.length === 0) {
+    return next(new AppError("No items found for this type", 404));
+  }
+  res.status(200).json({
+    status: "success",
+    results: items.length,
+    data: {
+      items,
+    },
+  });
+});
