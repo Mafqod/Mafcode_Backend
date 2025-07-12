55f532914a5e451a2a4d58574966a583ca489a62
nashaat10 <94199891+nashaat10@users.noreply.github.com>
2025-07-12 18:20:59 +0300

    add item model

---
+++ src/models/itemModel.js
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+const itemSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: [true, "Description is required"],
+      trim: true,
+    },
+    model: {
+      type: String,
+      required: [true, "Model is required"],
+      trim: true,
+    },
+    color: {
+      type: String,
+      required: [true, "Color is required"],
+      trim: true,
+    },
+    type: {
+      type: String,
+      required: [true, "Type is required"],
+      trim: true,
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      versionKey: false,
+      transform: function (doc, ret) {
+        delete ret._id;
+        return ret;
+      },
+    },
+    toObject: {
+      virtuals: true,
+      versionKey: false,
+      transform: function (doc, ret) {
+        delete ret._id;
+        return ret;
+      },
+    },
+  }
+);
+
+const Item = mongoose.model("Item", itemSchema);
+export default Item;
