5ae4f261f42c8c2ed3b21671e0155da37416d428
Mohamed Nashaat <94199891+nashaat10@users.noreply.github.com>
2025-08-03 01:19:51 +0300

    update port

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ mongoose
   })
   .catch((error) => console.log("database error", error.message));
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 9000;
 
 app.listen(port, () => {
   console.log("Server is running on port " + port);
